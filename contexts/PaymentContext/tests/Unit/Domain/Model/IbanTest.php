4eed2467d5a92420f2a92d68235d1a55f34d2b6e
Pablo Grass <pablo.grass@wikimedia.de>
2017-12-29 16:33:16 +0100

    strip all non-alnum chars from ibans

--- contexts/PaymentContext/tests/Unit/Domain/Model/IbanTest.php
+++ contexts/PaymentContext/tests/Unit/Domain/Model/IbanTest.php
@@ -18,9 +18,22 @@ class IbanTest extends \PHPUnit\Framework\TestCase {
 	const TEST_IBAN = 'DE12500105170648489890';
 	const TEST_LOWERCASE_IBAN = 'de12500105170648489890';
 
-	public function testGivenIbanWithWhitespace_WhitespaceIsRemoved(): void {
-		$iban = new Iban( self::TEST_IBAN_WITH_WHITESPACE );
-		$this->assertSame( self::TEST_IBAN, $iban->toString() );
+	/**
+	 * @dataProvider getValidIbansWithDisallowedCharacters
+	 */
+	public function testGivenIbanWithDisallowedCharacters_onlySaneCharactersAreConsidered( string $input, string $expected ): void {
+		$iban = new Iban( $input );
+		$this->assertSame( $expected, $iban->toString() );
+	}
+
+	public function getValidIbansWithDisallowedCharacters(): array {
+		return [
+			[ "AT\xc2\xa7022050302101023600", 'AT022050302101023600' ],
+			[ "DE\xe2\x80\xaa1250010517064\xe2\x80\xac8489890", 'DE12500105170648489890' ],
+			[ self::TEST_IBAN_WITH_WHITESPACE, self::TEST_IBAN ],
+			[ 'CH17  12341234  1234123419', 'CH17123412341234123419' ],
+			[ '  DE12500105170648489890 ', 'DE12500105170648489890' ]
+		];
 	}
 
 	public function testCountryCodeIsReturnedCorrectly(): void {
