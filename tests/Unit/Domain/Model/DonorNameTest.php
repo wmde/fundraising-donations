f07e3472adebc6f8bd31c0f3088a06c99344fdef
Gabriel Birke <gabriel.birke@wikimedia.de>
2018-01-11 17:17:41 +0100

    Refactor DonorNameTest
    Split test cases into methods, describing the expected concatenation
    behavior with sentences. Combinations of person and company name are
    unlikely to happen in the current application but might come up in the
    future.
    
    Add test cases for company names, they were missing before.

--- tests/Unit/Domain/Model/DonorNameTest.php
+++ tests/Unit/Domain/Model/DonorNameTest.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Frontend\DonationContext\Tests\Unit\Domain\Model;
 
+use PHPUnit\Framework\TestCase;
 use WMDE\Fundraising\Frontend\DonationContext\Domain\Model\DonorName;
 
 /**
@@ -11,71 +12,80 @@ use WMDE\Fundraising\Frontend\DonationContext\Domain\Model\DonorName;
  *
  * @license GNU GPL v2+
  * @author Kai Nissen < kai.nissen@wikimedia.de >
+ * @author Gabriel Birke < gabriel.birke@wikimedia.de >
  */
-class DonorNameTest extends \PHPUnit\Framework\TestCase {
+class DonorNameTest extends TestCase {
 
-	/**
-	 * @dataProvider privatePersonProvider
-	 */
-	public function testGivenPersonName_determineFullNameReturnsFullName( string $expectedValue, array $data ): void {
+	public function testGivenPersonNameWithoutSalutation_getFullNameReturnsNameWithoutSalutation(): void {
 		$personName = DonorName::newPrivatePersonName();
 
-		$personName->setCompanyName( $data['company'] );
-		$personName->setFirstName( $data['firstName'] );
-		$personName->setLastName( $data['lastName'] );
-		$personName->setTitle( $data['title'] );
+		$personName->setFirstName( 'Ebenezer' );
+		$personName->setLastName( 'Scrooge' );
 
-		$this->assertSame( $expectedValue, $personName->getFullName() );
+		$this->assertSame( 'Ebenezer Scrooge', $personName->getFullName() );
 	}
 
-	public function privatePersonProvider(): array {
-		return [
-			[
-				'Ebenezer Scrooge',
-				[
-					'title' => '',
-					'firstName' => 'Ebenezer',
-					'lastName' => 'Scrooge',
-					'company' => ''
-				]
-			],
-			[
-				'Sir Ebenezer Scrooge',
-				[
-					'title' => 'Sir',
-					'firstName' => 'Ebenezer',
-					'lastName' => 'Scrooge',
-					'company' => ''
-				]
-			],
-			[
-				'Prof. Dr. Friedemann Schulz von Thun',
-				[
-					'title' => 'Prof. Dr.',
-					'firstName' => 'Friedemann',
-					'lastName' => 'Schulz von Thun',
-					'company' => ''
-				]
-			],
-			[
-				'Hank Scorpio, Globex Corp.',
-				[
-					'title' => '',
-					'firstName' => 'Hank',
-					'lastName' => 'Scorpio',
-					'company' => 'Globex Corp.'
-				]
-			],
-			[
-				'Evil Hank Scorpio, Globex Corp.',
-				[
-					'title' => 'Evil',
-					'firstName' => 'Hank',
-					'lastName' => 'Scorpio',
-					'company' => 'Globex Corp.'
-				]
-			]
-		];
+	public function testGivenPersonNameWithSalutation_getFullNameReturnsNameWithoutSalutation(): void {
+		$personName = DonorName::newPrivatePersonName();
+
+		$personName->setFirstName( 'Ebenezer' );
+		$personName->setLastName( 'Scrooge' );
+		$personName->setSalutation( 'Sir' );
+
+		$this->assertSame( 'Ebenezer Scrooge', $personName->getFullName() );
+	}
+
+	public function testGivenPersonNameWithTitle_getFullNameReturnsNameWithTitle(): void {
+		$personName = DonorName::newPrivatePersonName();
+
+		$personName->setFirstName( 'Friedemann' );
+		$personName->setLastName( 'Schulz von Thun' );
+		$personName->setTitle( 'Prof. Dr.' );
+
+		$this->assertSame( 'Prof. Dr. Friedemann Schulz von Thun', $personName->getFullName() );
+	}
+
+	public function testGivenPersonNameWithTitleAndSalutation_getFullNameReturnsNameWithTitle(): void {
+		$personName = DonorName::newPrivatePersonName();
+
+		$personName->setFirstName( 'Friedemann' );
+		$personName->setLastName( 'Schulz von Thun' );
+		$personName->setTitle( 'Prof. Dr.' );
+		$personName->setSalutation( 'Herr' );
+
+		$this->assertSame( 'Prof. Dr. Friedemann Schulz von Thun', $personName->getFullName() );
+	}
+
+	public function testGivenPersonNameWithTitleAndCompay_getFullNameReturnsNameWithTitleAndCompany(): void {
+		$personName = DonorName::newPrivatePersonName();
+
+		$personName->setFirstName( 'Hank' );
+		$personName->setLastName( 'Scorpio' );
+		$personName->setCompanyName( 'Globex Corp.' );
+		$personName->setTitle( 'Evil Genius' );
+		$personName->setSalutation( 'Mr.' );
+
+		$this->assertSame( 'Evil Genius Hank Scorpio, Globex Corp.', $personName->getFullName() );
+	}
+
+	public function testGivenCompanyNameWithPerson_getFullNameReturnsPersonAndCompanyName(): void {
+		$companyName = DonorName::newCompanyName();
+
+		$companyName->setFirstName( 'Hank' );
+		$companyName->setLastName( 'Scorpio' );
+		$companyName->setCompanyName( 'Globex Corp.' );
+		$companyName->setTitle( 'Evil Genius' );
+		$companyName->setSalutation( 'Mr.' );
+
+		$this->assertSame( 'Evil Genius Hank Scorpio, Globex Corp.', $companyName->getFullName() );
+	}
+
+	public function testGivenCompanyNameOnly_getFullNameReturnsCompanyName(): void {
+		$companyName = DonorName::newCompanyName();
+
+		$companyName->setCompanyName( 'Globex Corp.' );
+
+		$this->assertSame( 'Globex Corp.', $companyName->getFullName() );
 	}
 
 }
