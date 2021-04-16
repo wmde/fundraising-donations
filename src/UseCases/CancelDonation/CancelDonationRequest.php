fdab43726c988d9e1686c6554e212e71b8468cdc
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2021-04-16 19:04:15 +0200

    Add cancellation use case
    - re-uses existing CancelDonationUseCase
    - extends the use case to also serve for cancellation requests of administrative users (FOC)
    
    https://phabricator.wikimedia.org/T276817

--- src/UseCases/CancelDonation/CancelDonationRequest.php
+++ src/UseCases/CancelDonation/CancelDonationRequest.php
@@ -10,14 +10,27 @@ namespace WMDE\Fundraising\DonationContext\UseCases\CancelDonation;
  */
 class CancelDonationRequest {
 
-	private $donationId;
+	private int $donationId;
+	private ?string $authorizedUser;
 
-	public function __construct( int $donationId ) {
+	public function __construct( int $donationId, ?string $authorizedUser = null ) {
 		$this->donationId = $donationId;
+		$this->authorizedUser = $authorizedUser;
 	}
 
 	public function getDonationId(): int {
 		return $this->donationId;
+	}
+
+	public function isAuthorizedRequest(): bool {
+		return $this->authorizedUser !== null;
+	}
+
+	public function getUserName(): string {
+		if ( $this->authorizedUser == null ) {
+			throw new \LogicException( "Tried to access user name of unauthorized user. Call isAuthorizedRequest() first!" );
+		}
+		return $this->authorizedUser;
 	}
 
 }
