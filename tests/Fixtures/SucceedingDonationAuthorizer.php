10325aa048d37c0f0df2a683ab0842ebe714d535
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2021-05-03 17:58:35 +0200

    Adds HttpDonationConfirmationNotifier
    - implements DonationConfirmationNotifier
    - calls http endpoint instead of sending mails directly
    
    - reason:
      We don't have templates set up in the FOC (fundraising operation center) and need to
    relay the sending of mails to the fundraising app.
    
    Related to this ticket:
    https://phabricator.wikimedia.org/T276820

--- tests/Fixtures/SucceedingDonationAuthorizer.php
+++ tests/Fixtures/SucceedingDonationAuthorizer.php
@@ -4,13 +4,21 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\DonationContext\Tests\Fixtures;
 
+use TheSeer\Tokenizer\Token;
 use WMDE\Fundraising\DonationContext\Authorization\DonationAuthorizer;
+use WMDE\Fundraising\DonationContext\Authorization\TokenSet;
 
 /**
  * @license GPL-2.0-or-later
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class SucceedingDonationAuthorizer implements DonationAuthorizer {
+
+	private TokenSet $tokenSet;
+
+	public function __construct() {
+		$this->tokenSet = new TokenSet( '~succeeding update token~', '~succeeding access token~' );
+	}
 
 	public function userCanModifyDonation( int $donationId ): bool {
 		return true;
@@ -24,4 +32,11 @@ class SucceedingDonationAuthorizer implements DonationAuthorizer {
 		return true;
 	}
 
+	public function getTokensForDonation( int $donationId ): TokenSet {
+		return $this->tokenSet;
+	}
+
+	public function setTokenSet( TokenSet $tokenSet ): void {
+		$this->tokenSet = $tokenSet;
+	}
 }
