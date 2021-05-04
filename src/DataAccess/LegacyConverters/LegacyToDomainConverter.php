bd8a76756f8d9e4609b1256e1278923a6e8724e2
Gabriel Birke <gabriel.birke@gmail.com>
2021-05-04 16:03:03 +0200

    Add missing state conversion
    Add state conversion for all payments to LegacyToDomainConverter
    
    While working on the conversion, discovered that tracking state is no
    longer necessary in the domain and created an issue. See
    https://phabricator.wikimedia.org/T281853
    
    This is for https://phabricator.wikimedia.org/T276817

--- src/DataAccess/LegacyConverters/LegacyToDomainConverter.php
+++ src/DataAccess/LegacyConverters/LegacyToDomainConverter.php
@@ -44,16 +44,28 @@ class LegacyToDomainConverter {
 		return $donation;
 	}
 
+	/**
+	 * Create a new status from the payment type.
+	 *
+	 * This method is a violation of the Open-Closed principle because we need to touch it whenever we add new payment types.
+	 * We are planning to remove the status from the Donation domain model all together,
+	 * see https://phabricator.wikimedia.org/T281853
+	 *
+	 * @param DoctrineDonation $dd
+	 *
+	 * @return string
+	 */
 	private function convertStatus( DoctrineDonation $dd ): string {
-		switch ( $dd->getPaymentType() ) {
-			case PaymentMethod::BANK_TRANSFER:
-				return Donation::STATUS_PROMISE;
-			case PaymentMethod::DIRECT_DEBIT:
-				return Donation::STATUS_NEW;
-			default:
-				// TODO ask Kai what to do with old data that would create an error here
-				return $dd->getStatus();
+		$paymentMethod = $this->getPaymentMethodFromEntity( $dd );
+		if ( $paymentMethod instanceof BankTransferPayment ) {
+			return Donation::STATUS_PROMISE;
+		} elseif ( $paymentMethod instanceof DirectDebitPayment ) {
+			return Donation::STATUS_NEW;
+		} elseif ( $paymentMethod->hasExternalProvider() ) {
+			return $paymentMethod->paymentCompleted() ? Donation::STATUS_EXTERNAL_BOOKED : Donation::STATUS_EXTERNAL_INCOMPLETE;
 		}
+		// For greater legacy compatibility (at the cost of future errors), we don't throw an exception here
+		return Donation::STATUS_PROMISE;
 	}
 
 	private function assignCancellationAndModeration( DoctrineDonation $dd, Donation $donation ): void {
