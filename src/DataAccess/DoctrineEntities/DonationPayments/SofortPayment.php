2e8a5cc9656e2582c257cc63abf3646cf2065ab4
Abban Dunne <abban.dunne@wikimedia.de>
2021-09-27 09:49:42 +0200

    Convert Doctrine mappings to xml

--- src/DataAccess/DoctrineEntities/DonationPayments/SofortPayment.php
+++ src/DataAccess/DoctrineEntities/DonationPayments/SofortPayment.php
@@ -5,20 +5,10 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\DonationContext\DataAccess\DoctrineEntities\DonationPayments;
 
 use DateTime;
-use Doctrine\ORM\Mapping as ORM;
 use WMDE\Fundraising\DonationContext\DataAccess\DoctrineEntities\DonationPayment;
 
-/**
- * @ORM\Table(name="donation_payment_sofort")
- * @ORM\Entity
- */
 class SofortPayment extends DonationPayment {
 
-	/**
-	 * @var DateTime|null
-	 *
-	 * @ORM\Column(name="confirmed_at", type="datetime", nullable=true)
-	 */
 	private $confirmedAt;
 
 	public function getConfirmedAt(): ?DateTime {
