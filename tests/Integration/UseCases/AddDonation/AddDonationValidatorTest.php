613a6046ec2734dd913e2bdf445e0a96b361466b
Gabriel Birke <gabriel.birke@posteo.net>
2022-05-03 14:44:06 +0200

    Adapt AddDonationValidator
    The validator will no longer validate payment data - that's the task of
    the CreatePaymentUseCase

--- tests/Integration/UseCases/AddDonation/AddDonationValidatorTest.php
+++ tests/Integration/UseCases/AddDonation/AddDonationValidatorTest.php
@@ -9,47 +9,31 @@ use WMDE\Euro\Euro;
 use WMDE\Fundraising\DonationContext\Domain\Model\DonorType;
 use WMDE\Fundraising\DonationContext\Tests\Data\ValidAddDonationRequest;
 use WMDE\Fundraising\DonationContext\Tests\Data\ValidatorPatterns;
-use WMDE\Fundraising\DonationContext\Tests\Data\ValidDonation;
 use WMDE\Fundraising\DonationContext\UseCases\AddDonation\AddDonationValidationResult;
 use WMDE\Fundraising\DonationContext\UseCases\AddDonation\AddDonationValidator;
-use WMDE\Fundraising\PaymentContext\Domain\BankDataValidationResult;
-use WMDE\Fundraising\PaymentContext\Domain\BankDataValidator;
-use WMDE\Fundraising\PaymentContext\Domain\DomainSpecificPaymentValidator;
-use WMDE\Fundraising\PaymentContext\Domain\IbanBlockList;
-use WMDE\Fundraising\PaymentContext\Domain\IbanValidator;
-use WMDE\Fundraising\PaymentContext\Domain\Model\PaymentInterval;
-use WMDE\Fundraising\PaymentContext\Domain\PaymentValidator;
 use WMDE\FunValidators\ConstraintViolation;
 use WMDE\FunValidators\SucceedingDomainNameValidator;
-use WMDE\FunValidators\ValidationResponse;
 use WMDE\FunValidators\ValidationResult;
 use WMDE\FunValidators\Validators\AddressValidator;
 use WMDE\FunValidators\Validators\EmailValidator;
 
 /**
  * @covers \WMDE\Fundraising\DonationContext\UseCases\AddDonation\AddDonationValidator
- *
- * @license GPL-2.0-or-later
- * @author Kai Nissen < kai.nissen@wikimedia.de >
- * @author Gabriel Birke < gabriel.birke@wikimedia.de >
  */
 class AddDonationValidatorTest extends TestCase {
 
 	private AddDonationValidator $donationValidator;
 
 	public function setUp(): void {
-		// TODO constructor currently throws an exception, reactivate this when the dependencies have been fixed
-		//$this->donationValidator = $this->newDonationValidator();
+		$this->donationValidator = $this->newDonationValidator();
 	}
 
 	public function testGivenValidDonation_validationIsSuccessful(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$request = ValidAddDonationRequest::getRequest();
 		$this->assertEmpty( $this->donationValidator->validate( $request )->getViolations() );
 	}
 
 	public function testGivenAnonymousDonorAndEmptyAddressFields_validatorReturnsNoViolations(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$request = ValidAddDonationRequest::getRequest();
 
 		$request->setDonorType( DonorType::ANONYMOUS() );
@@ -67,34 +51,7 @@ class AddDonationValidatorTest extends TestCase {
 		$this->assertEmpty( $this->donationValidator->validate( $request )->getViolations() );
 	}
 
-	public function testGivenNoPaymentType_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
-		$request = ValidAddDonationRequest::getRequest();
-		$request->setPaymentType( '' );
-
-		$this->assertFalse( $this->donationValidator->validate( $request )->isSuccessful() );
-
-		$this->assertConstraintWasViolated(
-			$this->donationValidator->validate( $request ),
-			AddDonationValidationResult::SOURCE_PAYMENT_TYPE
-		);
-	}
-
-	public function testGivenUnsupportedPaymentType_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
-		$request = ValidAddDonationRequest::getRequest();
-		$request->setPaymentType( 'KaiCoin' );
-
-		$this->assertFalse( $this->donationValidator->validate( $request )->isSuccessful() );
-
-		$this->assertConstraintWasViolated(
-			$this->donationValidator->validate( $request ),
-			AddDonationValidationResult::SOURCE_PAYMENT_TYPE
-		);
-	}
-
 	public function testPersonalInfoValidationFails_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$request = ValidAddDonationRequest::getRequest();
 		$request->setDonorType( DonorType::COMPANY() );
 		$request->setDonorCompany( '' );
@@ -107,50 +64,8 @@ class AddDonationValidatorTest extends TestCase {
 		);
 	}
 
-	public function testGivenFailingBankDataValidator_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
-		$bankDataValidator = $this->createMock( BankDataValidator::class );
-		$bankDataValidator->method( 'validate' )->willReturn( new ValidationResult(
-			new ConstraintViolation(
-				'',
-				BankDataValidationResult::VIOLATION_MISSING,
-				BankDataValidationResult::SOURCE_IBAN
-			)
-		) );
-		$validator = new AddDonationValidator(
-			new PaymentValidator( $this->newPassingDomainSpecificValidator() ),
-			$bankDataValidator,
-			$this->newEmptyIbanBlockList(),
-			$this->newEmailValidator(),
-			$this->newAddressValidator()
-		);
-		$request = ValidAddDonationRequest::getRequest();
-
-		$result = $validator->validate( $request );
-		$this->assertFalse( $result->isSuccessful() );
-
-		$this->assertConstraintWasViolated( $result, BankDataValidationResult::SOURCE_IBAN );
-	}
-
-	public function testGivenBlockedIban_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'IBAN blocklist checking should be part of the payment validation, remove this test when that\'s been implemented' );
-		$validator = new AddDonationValidator(
-			new PaymentValidator( $this->newPassingDomainSpecificValidator() ),
-			$this->newBankDataValidator(),
-			new IbanBlocklist( [ ValidDonation::PAYMENT_IBAN ] ),
-			$this->newEmailValidator(),
-			$this->newAddressValidator()
-		);
-		$request = ValidAddDonationRequest::getRequest();
-
-		$result = $validator->validate( $request );
-		$this->assertFalse( $result->isSuccessful() );
-
-		$this->assertConstraintWasViolated( $result, BankDataValidationResult::SOURCE_IBAN );
-	}
-
 	public function testAmountTooLow_validatorReturnsFalse(): void {
-		$this->markTestIncomplete( 'TODO: Create DomainSpecificPaymentValidator implementation for amount checking' );
+		$this->markTestIncomplete( 'TODO: Create DomainSpecificPaymentValidator implementation for amount checking, move this test to Fundraising App integration test' );
 		$request = ValidAddDonationRequest::getRequest();
 		$request->setAmountInEuroCents( Euro::newFromCents( 50 ) );
 
@@ -161,7 +76,6 @@ class AddDonationValidatorTest extends TestCase {
 	}
 
 	public function testDonorWithLongFields_validationFails(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$longText = str_repeat( 'Cats ', 500 );
 		$request = ValidAddDonationRequest::getRequest();
 		$request->setDonorFirstName( $longText );
@@ -189,7 +103,6 @@ class AddDonationValidatorTest extends TestCase {
 	}
 
 	public function testGivenEmailOnlyDonorWithMissingNameParts_validationFails(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$request = ValidAddDonationRequest::getRequest();
 
 		$request->setDonorType( DonorType::EMAIL() );
@@ -206,7 +119,6 @@ class AddDonationValidatorTest extends TestCase {
 	}
 
 	public function testGivenCompleteEmailOnlyDonor_validationSucceeds(): void {
-		$this->markTestIncomplete( 'This should work when we changed the amount field in request to int and removed the error' );
 		$request = ValidAddDonationRequest::getRequest();
 
 		$request->setDonorType( DonorType::EMAIL() );
@@ -220,20 +132,9 @@ class AddDonationValidatorTest extends TestCase {
 
 	private function newDonationValidator(): AddDonationValidator {
 		return new AddDonationValidator(
-			new PaymentValidator( $this->newPassingDomainSpecificValidator() ),
-			$this->newBankDataValidator(),
-			$this->newEmptyIbanBlockList(),
 			$this->newEmailValidator(),
 			$this->newAddressValidator()
 		);
-	}
-
-	private function newBankDataValidator(): BankDataValidator {
-		$ibanValidatorMock = $this->getMockBuilder( IbanValidator::class )->disableOriginalConstructor()->getMock();
-		$ibanValidatorMock->method( 'validate' )
-			->willReturn( new ValidationResult() );
-
-		return new BankDataValidator( $ibanValidatorMock );
 	}
 
 	private function newEmailValidator(): EmailValidator {
@@ -256,21 +157,7 @@ class AddDonationValidatorTest extends TestCase {
 		);
 	}
 
-	private function newEmptyIbanBlockList(): IbanBlockList {
-		return new IbanBlockList( [] );
-	}
-
 	private function newAddressValidator(): AddressValidator {
 		return new AddressValidator( ValidatorPatterns::COUNTRY_POSTCODE, ValidatorPatterns::ADDRESS_PATTERNS );
 	}
-
-	private function newPassingDomainSpecificValidator(): DomainSpecificPaymentValidator {
-		return new class implements DomainSpecificPaymentValidator {
-			public function validatePaymentData( Euro $amount, PaymentInterval $interval ): ValidationResponse {
-				return ValidationResponse::newSuccessResponse();
-			}
-
-		};
-	}
-
 }
