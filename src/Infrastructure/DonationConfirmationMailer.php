7fcd7d928f70799cbd341c28ad04e33abae92760
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-05-10 16:18:02 +0200

    Fix confirmation mailer
    Fetch payment data with use case
    
    Co-Autored-By: Abban Dunne <abban.dunne@wikimedia.de>

--- src/Infrastructure/DonationConfirmationMailer.php
+++ src/Infrastructure/DonationConfirmationMailer.php
@@ -7,16 +7,17 @@ namespace WMDE\Fundraising\DonationContext\Infrastructure;
 use WMDE\EmailAddress\EmailAddress;
 use WMDE\Fundraising\DonationContext\Domain\Model\Donation;
 use WMDE\Fundraising\DonationContext\UseCases\DonationConfirmationNotifier;
+use WMDE\Fundraising\PaymentContext\UseCases\GetPayment\GetPaymentUseCase;
 
 /**
  * @license GPL-2.0-or-later
  */
 class DonationConfirmationMailer implements DonationConfirmationNotifier {
 
-	private TemplateMailerInterface $mailer;
-
-	public function __construct( TemplateMailerInterface $mailer ) {
-		$this->mailer = $mailer;
+	public function __construct(
+		private readonly TemplateMailerInterface $mailer,
+		private readonly GetPaymentUseCase $getPaymentService
+	) {
 	}
 
 	public function sendConfirmationFor( Donation $donation ): void {
@@ -30,17 +31,16 @@ class DonationConfirmationMailer implements DonationConfirmationNotifier {
 	}
 
 	private function getConfirmationMailTemplateArguments( Donation $donation ): array {
+		$paymentInfo = $this->getPaymentService->getPaymentDataArray( $donation->getPaymentId() );
 		return [
 			'recipient' => $donation->getDonor()->getName()->toArray(),
 			'donation' => [
 				'id' => $donation->getId(),
-				// TODO use "get payment" use case for getting payment information
-				'amount' => $donation->getAmount()->getEuroFloat(),
-				'interval' => $donation->getPaymentIntervalInMonths(),
-				'paymentType' => $donation->getPaymentMethodId(),
+				'amount' => $paymentInfo['amount'],
+				'interval' => $paymentInfo['interval'],
+				'paymentType' => $paymentInfo['paymentType'],
 				'needsModeration' => $donation->isMarkedForModeration(),
-				// TODO use "get payment" use case for getting transfer code, default to empty string
-				'bankTransferCode' => '',
+				'bankTransferCode' => $paymentInfo['ueb_code'],
 				'receiptOptIn' => $donation->getOptsIntoDonationReceipt(),
 			]
 		];
