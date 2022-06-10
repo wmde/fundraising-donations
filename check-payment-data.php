b2de73fe24859b481d7e56e67b535d1b28e32f5f
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-06-10 18:17:51 +0200

    Output child IDs PayPal double bookings
    When checking the data, output the child donation IDs of PayPal
    donations that were double-booked
    (see https://github.com/wmde/fundraising-donations/pull/81 ).
    
    We'll delete these donations before running the data migration.

--- check-payment-data.php
+++ check-payment-data.php
@@ -26,9 +26,6 @@ $processedPayments = $result->getDonationCount();
 $errorCount = array_reduce($errors, fn(int $acc, ResultObject $error) => $acc + $error->getItemCount(), 0 );
 $warningCount = array_reduce($warnings, fn(int $acc, ResultObject $error) => $acc + $error->getItemCount(), 0 );
 
-// TODO Move counts into AnalysisResult and get more accurate warning count by ignoring duplicate donation IDs?
-//    One donation can have multiple warnings, e.g. paypal payments without payer id and timestamp
-
 printf( "\nProcessed %d donations, with %d errors (%.2f%%) and %d warnings (%.2f%%)\n",
 	$processedPayments,
 	$errorCount,
@@ -48,6 +45,15 @@ foreach($warnings as $type => $warning) {
 	$percentageOfDonations = ( $warningCount * 100 ) / $processedPayments;
 	printf("|%-60s: | %d (%.2f%%) | (%s - %s) |\n", $type, $warningCount, $percentageOfDonations, $lower->format('Y-m-d'), $upper->format('Y-m-d') );
 }
+
+$doubleBookings = $converter->getDoubleBookedPayPalChildIds();
+if(!empty($doubleBookings)) {
+	// Just for checking if the multi-booked parents have a different error source, but they don't.
+	// $multiChildParents = array_filter( $doubleBookings, fn($booking) => count($booking) > 1 );
+	// printf("\nMulti-booked PayPal parent IDs:\n%s\n", implode(", ", array_keys($multiChildParents)));
+	printf("\nDouble-Booked Paypal child donation IDs:\n%s\n", implode(", ", array_merge(...array_values($doubleBookings))));
+}
+
 
 if ( count($errors) === 0) {
 	echo "\nNo errors.\n";
