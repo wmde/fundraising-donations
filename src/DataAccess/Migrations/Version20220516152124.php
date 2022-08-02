3379d7ec57633a14d8f00d39d05e2e58a91efb77
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-08-02 18:33:27 +0200

    Improve migrations
    Add comments
    
    Remove duplicate dependency from composer.json after merge

--- src/DataAccess/Migrations/Version20220516152124.php
+++ src/DataAccess/Migrations/Version20220516152124.php
@@ -7,14 +7,25 @@ namespace WMDE\Fundraising\DonationContext\DataAccess\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+/**
+ * This migration prepares the donation table for using the new payment persistence tables.
+ *
+ * Since the payment data is not consistent and easily accessible, this is just modifying the structure,
+ * the data migration is in {@see \WMDE\Fundraising\DonationContext\DataAccess\PaymentMigration\DonationToPaymentConverter}
+ *
+ * After the data migration, run {@see \WMDE\Fundraising\DonationContext\DataAccess\Migrations\Version20220613145732}
+ */
 final class Version20220516152124 extends AbstractMigration {
 	public function getDescription(): string {
 		return 'Use new payment domain for donations';
 	}
 
 	public function up( Schema $schema ): void {
+		// Rename the old bounded-context-specific payment_id to legacy
+		// We still need it for the data migration
 		$this->addSql( 'ALTER TABLE spenden CHANGE COLUMN payment_id legacy_payment_id INTEGER' );
-		// We'll have to add the unique index (not FK) in the next migration, because we don't have payments yet
+		// Add the new payment_id. Strictly speaking, this is a non-nullable foreign key, but we can't model it as such
+		// We'll add the unique index in the next migration to keep the data migration fast
 		$this->addSql( 'ALTER TABLE spenden ADD COLUMN payment_id INTEGER DEFAULT 0' );
 	}
 
--- src/DataAccess/Migrations/Version20220701153328.php
+++ src/DataAccess/Migrations/Version20220701153328.php
@@ -8,18 +8,23 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * This is a clean-up migration for the payment domain changes.
+ *
+ * You should run this when the payment has been migrated successfully
+ * (2 weeks or more after the migration, when we're really sure that everything works).
  */
 final class Version20220701153328 extends AbstractMigration {
 	public function getDescription(): string {
-		return '';
+		return 'Drop legacy payment tables';
 	}
 
 	public function up( Schema $schema ): void {
-		// this up() migration is auto-generated, please modify it to your needs
+		$this->addSql( 'ALTER TABLE spenden DROP COLUMN legacy_payment_id' );
+		$schema->dropTable( 'donation_payment' );
+		$schema->dropTable( 'donation_payment_sofort' );
 	}
 
 	public function down( Schema $schema ): void {
-		// this down() migration is auto-generated, please modify it to your needs
+		$this->throwIrreversibleMigrationException( "We can't reconstruct the legacy payment IDs. Please check your backup files." );
 	}
 }
