63cf9902e70b7c033ad12d0eda95aa6c1bdbfbbb
Sperling-0 <tanujadoriya@gmail.com>
2024-02-07 09:52:43 +0100

    Update Doctrine ORM
    - Resolve the errors
    
    Ticket: https://phabricator.wikimedia.org/T356744

--- src/Infrastructure/DonationEventLogException.php
+++ src/Infrastructure/DonationEventLogException.php
@@ -4,13 +4,16 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\DonationContext\Infrastructure;
 
+use RuntimeException;
+use Throwable;
+
 /**
  * @license GPL-2.0-or-later
  * @author Gabriel Birke < gabriel.birke@wikimedia.de >
  */
-class DonationEventLogException extends \RuntimeException {
+class DonationEventLogException extends RuntimeException {
 
-	public function __construct( string $message, \Exception $previous = null ) {
+	public function __construct( string $message, Throwable $previous = null ) {
 		parent::__construct( $message, 0, $previous );
 	}
 
