57c6a54e28028c5fe80ff7a75b294d079b418b5e
Abban Dunne <abban.dunne@wikimedia.de>
2024-03-05 15:11:18 +0100

    Remove freezable trait from AddCommentRequest
    - Updated tests
    - Removed the freezable trait composer dependency
    
    Ticket: https://phabricator.wikimedia.org/T359090

--- src/UseCases/AddComment/AddCommentRequest.php
+++ src/UseCases/AddComment/AddCommentRequest.php
@@ -4,65 +4,16 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\DonationContext\UseCases\AddComment;
 
-use WMDE\FreezableValueObject\FreezableValueObject;
-
-/**
- * @license GPL-2.0-or-later
- * @author Gabriel Birke < gabriel.birke@wikimedia.de >
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- */
 class AddCommentRequest {
-	use FreezableValueObject;
 
-	private string $commentText;
-	private bool $isPublic;
-	private bool $isAnonymous;
-	private int $donationId;
+	public readonly string $commentText;
 
-	public function getCommentText(): string {
-		return $this->commentText;
-	}
-
-	public function setCommentText( string $commentText ): self {
-		$this->assertIsWritable();
+	public function __construct(
+		string $commentText,
+		public readonly bool $isPublic,
+		public readonly bool $isAnonymous,
+		public readonly int $donationId
+	) {
 		$this->commentText = trim( $commentText );
-		return $this;
 	}
-
-	public function isPublic(): bool {
-		return $this->isPublic;
-	}
-
-	public function setIsPublic( bool $isPublic ): self {
-		$this->assertIsWritable();
-		$this->isPublic = $isPublic;
-		return $this;
-	}
-
-	public function isAnonymous(): bool {
-		return $this->isAnonymous;
-	}
-
-	public function setIsAnonymous(): self {
-		$this->assertIsWritable();
-		$this->isAnonymous = true;
-		return $this;
-	}
-
-	public function setIsNamed(): self {
-		$this->assertIsWritable();
-		$this->isAnonymous = false;
-		return $this;
-	}
-
-	public function getDonationId(): int {
-		return $this->donationId;
-	}
-
-	public function setDonationId( int $donationId ): self {
-		$this->assertIsWritable();
-		$this->donationId = $donationId;
-		return $this;
-	}
-
 }
