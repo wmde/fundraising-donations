f92c5b0373fce223f170cb4075a8353431ec8805
Gabriel Birke <gabriel.birke@wikimedia.de>
2024-03-19 17:15:57 +0100

    Clean up CancelDonationUseCase
    Since user can no longer cancel donations, remove all code that
    distinguishes between users and administrators.
    
    Remove reference to mailer interface, since that is no longer needed.
    
    This'll help improving the TemplateMailerInterface to accept only
    objects.
    
    Add `allow-plugins` section to composer.json to allow for new code
    style.
    
    The changes in the CancelDonationUseCase and CancelDonationRequest
    constructor are a BC break.

--- src/UseCases/CancelDonation/CancelDonationUseCase.php
+++ src/UseCases/CancelDonation/CancelDonationUseCase.php
@@ -4,28 +4,24 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\DonationContext\UseCases\CancelDonation;
 
-use WMDE\EmailAddress\EmailAddress;
-use WMDE\Fundraising\DonationContext\Domain\Model\Donation;
 use WMDE\Fundraising\DonationContext\Domain\Repositories\DonationRepository;
 use WMDE\Fundraising\DonationContext\Domain\Repositories\GetDonationException;
 use WMDE\Fundraising\DonationContext\Domain\Repositories\StoreDonationException;
 use WMDE\Fundraising\DonationContext\Infrastructure\DonationAuthorizationChecker;
 use WMDE\Fundraising\DonationContext\Infrastructure\DonationEventLogger;
-use WMDE\Fundraising\DonationContext\Infrastructure\TemplateMailerInterface;
 use WMDE\Fundraising\PaymentContext\UseCases\CancelPayment\CancelPaymentUseCase;
 use WMDE\Fundraising\PaymentContext\UseCases\CancelPayment\FailureResponse;
 
 class CancelDonationUseCase {
 
-	private const LOG_MESSAGE_DONATION_STATUS_CHANGE = 'frontend: storno';
 	private const LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN = 'cancelled by user: %s';
 
 	public function __construct(
-		private DonationRepository $donationRepository,
-		private TemplateMailerInterface $mailer,
-		private DonationAuthorizationChecker $authorizationService,
-		private DonationEventLogger $donationLogger,
-		private CancelPaymentUseCase $cancelPaymentUseCase ) {
+		private readonly DonationRepository $donationRepository,
+		private readonly DonationAuthorizationChecker $authorizationService,
+		private readonly DonationEventLogger $donationLogger,
+		private readonly CancelPaymentUseCase $cancelPaymentUseCase
+	) {
 	}
 
 	public function cancelDonation( CancelDonationRequest $cancellationRequest ): CancelDonationResponse {
@@ -58,23 +54,11 @@ class CancelDonationUseCase {
 
 		$this->donationLogger->log( $donation->getId(), $this->getLogMessage( $cancellationRequest ) );
 
-		try {
-			$this->sendConfirmationEmail( $cancellationRequest, $donation );
-		} catch ( \RuntimeException $ex ) {
-			return new CancelDonationResponse(
-				$cancellationRequest->getDonationId(),
-				CancelDonationResponse::MAIL_DELIVERY_FAILED
-			);
-		}
-
 		return $this->newSuccessResponse( $cancellationRequest );
 	}
 
 	public function getLogMessage( CancelDonationRequest $cancellationRequest ): string {
-		if ( $cancellationRequest->isAuthorizedRequest() ) {
-			return sprintf( self::LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN, $cancellationRequest->getUserName() );
-		}
-		return self::LOG_MESSAGE_DONATION_STATUS_CHANGE;
+		return sprintf( self::LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN, $cancellationRequest->getUserName() );
 	}
 
 	public function requestIsAllowedToModifyDonation( CancelDonationRequest $cancellationRequest ): bool {
@@ -82,7 +66,8 @@ class CancelDonationUseCase {
 			return $this->authorizationService->systemCanModifyDonation( $cancellationRequest->getDonationId() );
 
 		}
-		return $this->authorizationService->userCanModifyDonation( $cancellationRequest->getDonationId() );
+		// Users on the frontend are no longer allowed to cancel donations
+		return false;
 	}
 
 	private function newFailureResponse( CancelDonationRequest $cancellationRequest ): CancelDonationResponse {
@@ -97,31 +82,6 @@ class CancelDonationUseCase {
 			$cancellationRequest->getDonationId(),
 			CancelDonationResponse::SUCCESS
 		);
-	}
-
-	private function sendConfirmationEmail( CancelDonationRequest $cancellationRequest, Donation $donation ): void {
-		if ( $cancellationRequest->isAuthorizedRequest() ) {
-			return;
-		}
-		if ( !$donation->getDonor()->hasEmailAddress() ) {
-			return;
-		}
-		$this->mailer->sendMail(
-			new EmailAddress( $donation->getDonor()->getEmailAddress() ),
-			$this->getConfirmationMailTemplateArguments( $donation )
-		);
-	}
-
-	/**
-	 * @param Donation $donation
-	 *
-	 * @return array<string,mixed>
-	 */
-	private function getConfirmationMailTemplateArguments( Donation $donation ): array {
-		return [
-			'donationId' => $donation->getId(),
-			'recipient' => $donation->getDonor()->getName()->toArray(),
-		];
 	}
 
 }
