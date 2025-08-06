2fd1a8a5dfa781499bca7efa3032e9413e4e06e6
Gabriel Birke <gabriel.birke@wikimedia.de>
2025-08-06 11:12:55 +0200

    Change Return objects of RestoreDonationUseCase
    Create Success/Error union type with simple value objects.
    This enables us to check error reasons.
    
    Ticket: https://phabricator.wikimedia.org/T396597

--- src/UseCases/RestoreDonation/RestoreDonationUseCase.php
+++ src/UseCases/RestoreDonation/RestoreDonationUseCase.php
@@ -9,7 +9,7 @@ use WMDE\Fundraising\DonationContext\Infrastructure\DonationEventLogger;
 
 class RestoreDonationUseCase {
 
-	private const LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN = 'restored by user: %s';
+	private const string LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN = 'restored by user: %s';
 
 	private DonationRepository $donationRepository;
 	private DonationEventLogger $donationLogger;
@@ -19,33 +19,19 @@ class RestoreDonationUseCase {
 		$this->donationLogger = $donationLogger;
 	}
 
-	public function restoreCancelledDonation( int $donationId, string $authorizedUser ): RestoreDonationResponse {
+	public function restoreCancelledDonation( int $donationId, string $authorizedUser ): RestoreDonationSuccessResponse|RestoreDonationFailureResponse {
 		$donation = $this->donationRepository->getDonationById( $donationId );
 		if ( $donation === null ) {
-			return $this->newFailureResponse( $donationId );
+			return new RestoreDonationFailureResponse( $donationId, RestoreDonationFailureResponse::DONATION_NOT_FOUND );
 		}
 		if ( !$donation->isCancelled() ) {
-			return $this->newFailureResponse( $donationId );
+			return new RestoreDonationFailureResponse( $donationId, RestoreDonationFailureResponse::DONATION_NOT_CANCELED );
 		}
 
 		$donation->revokeCancellation();
 		$this->donationRepository->storeDonation( $donation );
 		$this->donationLogger->log( $donationId, sprintf( self::LOG_MESSAGE_DONATION_STATUS_CHANGE_BY_ADMIN, $authorizedUser ) );
 
-		return $this->newSuccessResponse( $donationId );
-	}
-
-	private function newFailureResponse( int $donationId ): RestoreDonationResponse {
-		return new RestoreDonationResponse(
-			$donationId,
-			RestoreDonationResponse::FAILURE
-		);
-	}
-
-	private function newSuccessResponse( int $donationId ): RestoreDonationResponse {
-		return new RestoreDonationResponse(
-			$donationId,
-			RestoreDonationResponse::SUCCESS
-		);
+		return new RestoreDonationSuccessResponse( $donationId );
 	}
 }
